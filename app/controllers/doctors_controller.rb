bbe11ba49a7c6b2e4141e5e666fea300cdd2fdc0
James Okunlade <james.okunlade@gmail.com>
2020-09-05 12:39:07 +0100

    Doctors tests and controller added

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,44 @@
 class DoctorsController < ApplicationController
+    before_action :set_doctor, only: [:show, :update, :destroy]
+
+    # GET /doctors
+    def index
+        @doctors = Doctor.all
+        json_response(@doctors)
+    end
+
+    # POST /doctors
+    def create
+        @doctor = Doctor.create!(doctor_params)
+        json_response(@doctor, :created)
+    end
+
+    # GET /doctors/:id
+    def show
+        json_response(@doctor)
+    end
+
+    # PUT /doctors/:id
+    def update
+        @doctor.update(doctor_params)
+        head :no_content
+    end
+
+    # DELETE /doctors/:id
+    def destroy
+        @doctor.destroy
+        head :no_content
+    end
+
+    private
+
+    def doctor_params
+        # whitelist
+        params.permit(:name, :email, :license, :address, :info, :years_of_experience, :phone, :specialty_id)
+    end
+
+    def set_doctor
+        @doctor = Doctor.find(params[:id])
+    end
+
 end
--- spec/requests/doctors_request_spec.rb
+++ spec/requests/doctors_request_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe "Doctors", type: :request do
+    let!(:specialty) { create(:specialty) }
+    let!(:doctors) { create_list(:doctor, 20, specialty_id: specialty.id) }
+    let(:doctor_id) { doctors.first.id }
+
+    # Test suite for GET /doctors
+    describe 'GET /doctors' do
+        # make HTTP get request before each example
+        before { get '/doctors' }
+
+        it 'returns doctors' do
+            # Json is a custom helper to parse JSON responses
+            expect(json).not_to be_empty
+            expect(json.size).to eq(20)
+        end
+
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    # Test suite for POST /doctors
+    describe 'POST /doctors' do
+        # valid payload
+        let(:valid_attributes) {{ name: 'Salako Omodunbi', email: 'salako@omodunbi@gmail.com', license: '123456', address: '24 Oluyoro Lagos', info: 'Medical doctor with 12 years of experience in consultancy', years_of_experience: 12, phone: '08175436794', specialty_id: specialty.id }}
+
+        context 'when the request is valid' do
+            before { post '/doctors', params: valid_attributes }
+
+            it 'creates a doctor' do
+                expect(json['name']).to eq('Salako Omodunbi')
+            end
+
+            it 'returns status code 201' do
+                expect(response).to have_http_status(201)
+            end
+        end
+
+        context 'when the request is invalid' do
+            before { post '/doctors', params: { name: 'Invalid doctor name'} }
+
+            it 'returns status code 422' do
+                expect(response).to have_http_status(422)
+            end
+        end
+    end
+
+    # Test suite for GET /doctors/:id
+    describe 'GET /doctors/:id' do
+        before { get "/doctors/#{doctor_id}" }
+
+        it 'returns a doctor' do
+            # Json is a custom helper to parse JSON responses
+            expect(json).not_to be_empty
+            # expect(json.size).to eq(1)
+        end
+
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    # Test suite for PUT /doctors/:id
+    describe 'PUT /doctors/:id' do
+        let(:valid_attributes) {{ name: 'Olutobi Rachel' }}
+
+        context 'when record exists' do
+            before { put "/doctors/#{doctor_id}", params: valid_attributes }
+
+            it 'updates the record' do
+                expect(response.body).to be_empty
+            end
+
+            it 'returns status code 204' do
+                expect(response).to have_http_status(204)
+            end
+        end
+    end
+
+    # Test suite for DELETE /doctors/:id
+    describe 'DELETE /doctors/:id' do
+        before { delete "/doctors/#{doctor_id}" }
+
+        it 'returns status code 204' do
+            expect(response).to have_http_status(204)
+        end
+    end
+
 
 end
