a6f696c8705627e91df2da08daea7167e52d8d9f
James Okunlade <james.okunlade@gmail.com>
2020-09-08 03:00:11 +0100

    added a has_many through appointments association to doctor and user model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,6 +2,7 @@ class Doctor < ApplicationRecord
   # Associations
   belongs_to :specialty
   has_many :appointments, dependent: :destroy
+  has_many :users, through: :appointments
 
   # Validations
   validates_presence_of :name, :email, :license, :address, :info, :years_of_experience, :phone
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     # Associations
     has_many :appointments, dependent: :destroy
+    has_many :doctors, through: :appointments
 
     # Validations
     validates_presence_of :name, :email
